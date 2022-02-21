9dd17f925238e0b4a61972fac7f2fe6705c29ff3
Nivelheint <sokolw21@yandex.by>
2022-02-21 14:32:56 +0300

    init: v1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplicationTest.Models;
+using Dapper;
+using Microsoft.Data;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace WebApplicationTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
+        public HomeController(ILogger<HomeController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        private List<User> GetAllUsers()
+        {
+            using (IDbConnection db = Connection)
+            {
+                var result = db.Query<User>("SELECT * FROM Users").ToList();
+                return result;
+            }
+        }
+
+        public IActionResult Index()
+        {
+            var items = GetAllUsers();
+            return View(items);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Messages()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplicationTest.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
